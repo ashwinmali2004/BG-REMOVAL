5f787c232e5348597141fa146cc4b79f604f3177
Ashwinmali2004 <Ashwinmali2004@gmail.com>
2025-01-09 22:57:55 +0530

    Some Updates

--- server/server.js
+++ server/server.js
@@ -1,26 +1,20 @@
-import 'dotenv/config';
-import express from 'express';
-import cors from 'cors';
-import connectDB from './configs/mongodb.js';
-// import userRouter from './routes/userRoutes.js';
+import 'dotenv/config'
+import express from 'express'
+import cors from 'cors'
+import connectDB from './configs/mongodb.js'
+// import userRouter from './routes/userRoutes.js'
 
 // App Config
-const PORT = process.env.PORT || 4000;
-const app = express();
-await connectDB();
+const PORT = process.env.PORT || 4000
+const app = express()
+await connectDB()
 
-// Initialize Middlewares
-app.use(express.json());
-app.use(cors());
+// Intialize Middlewares
+app.use(express.json())
+app.use(cors())
 
-// Handle /favicon.ico
-app.get('/favicon.ico', (req, res) => {
-  res.status(204).end(); // No Content
-});
+// API routes
+app.get('/',(req, res)=> res.send("API Working"))
+// app.use('/api/user', userRouter)
 
-// API Routes
-app.get('/', (req, res) => res.send("API Working"));
-// app.use('/api/user', userRouter);
-
-// Start Server
-app.listen(PORT, () => console.log("Server Running on port " + PORT));
+app.listen(PORT, ()=> console.log("Server Running on port "+ PORT))
