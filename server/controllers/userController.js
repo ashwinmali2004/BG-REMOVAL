44fad03a9b518978090557bc356281e36f3acd78
Ashwinmali2004 <Ashwinmali2004@gmail.com>
2025-01-28 21:57:27 +0530

    rechecking

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -5,66 +5,66 @@ import userModel from '../models/userModel.js'
 // http://localhost:4000/api/user/webhooks
 const clerkWebhooks = async(req,res)=>{
     
-    try{
+    // try{
 
-        // Create svix instance with clerk webhook secret
-        const whook = new Webhook(process.env.CLERK_WEBHOOK_SECRET)
+    //     // Create svix instance with clerk webhook secret
+    //     const whook = new Webhook(process.env.CLERK_WEBHOOK_SECRET)
 
-        await whook.verify(JSON.stringify(req.body),{
-            "svix-id":req.headers["svix-id"],
-            "svix-timestamp":req.headers["svix-timestamp"],
-            "svix-signature":req.headers["svix-signature"]
-        })
+    //     await whook.verify(JSON.stringify(req.body),{
+    //         "svix-id":req.headers["svix-id"],
+    //         "svix-timestamp":req.headers["svix-timestamp"],
+    //         "svix-signature":req.headers["svix-signature"]
+    //     })
 
-        const {data, type} = req.body
+    //     const {data, type} = req.body
 
-        switch(type){
-            case "user.created":{
+    //     switch(type){
+    //         case "user.created":{
 
-                const userData = {
-                    clerkId: data.id,
-                    email: data.email_addresses[0].email_address,
-                    firstName: data.first_name,
-                    lastName: data.last_name,
-                    photo: data.image_url
-                }
+    //             const userData = {
+    //                 clerkId: data.id,
+    //                 email: data.email_addresses[0].email_address,
+    //                 firstName: data.first_name,
+    //                 lastName: data.last_name,
+    //                 photo: data.image_url
+    //             }
 
-                await userModel.create(userData)
-                res.json({})
+    //             await userModel.create(userData)
+    //             res.json({})
 
-                break;
-            }
+    //             break;
+    //         }
 
-            case "user.updated":{
+    //         case "user.updated":{
 
-                const userData = {
-                    email: data.email_addresses[0].email_address,
-                    firstName: data.first_name,
-                    lastName: data.last_name,
-                    photo: data.image_url
-                }
+    //             const userData = {
+    //                 email: data.email_addresses[0].email_address,
+    //                 firstName: data.first_name,
+    //                 lastName: data.last_name,
+    //                 photo: data.image_url
+    //             }
 
-                await userModel.findOneAndUpdate({clerkId:data.id}, userData)
-                res.json({})
+    //             await userModel.findOneAndUpdate({clerkId:data.id}, userData)
+    //             res.json({})
 
-                break;
-            }
+    //             break;
+    //         }
 
-            case "user.deleted":{
+    //         case "user.deleted":{
 
-                await userModel.findOneAndDelete({clerkId:data.id})
-                res.json({})
+    //             await userModel.findOneAndDelete({clerkId:data.id})
+    //             res.json({})
 
-                break;
-            }
-            default:
-                break;
-        }
+    //             break;
+    //         }
+    //         default:
+    //             break;
+    //     }
 
-    }catch(error){
-        console.log(error.message)
-        res.json({success:false, message:error.message})
-    }
+    // }catch(error){
+    //     console.log(error.message)
+    //     res.json({success:false, message:error.message})
+    // }
 
 }
 
